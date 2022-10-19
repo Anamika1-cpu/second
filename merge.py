6200f0c43aa28ea49d7b2220820bd4d90e381f40
Poorvanshi-sahu <65068317+Poorvanshi-sahu@users.noreply.github.com>
2022-10-19 05:37:00 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,22 @@
+#Merge two sorted array
+def merge(a,b):
+     lena=len(a);
+     lenb=len(b);
+     print(lena,lenb)
+     i=0;
+     j=0;
+     c=list();
+     while(i<lena and j<lenb):
+         if a[i]<=b[j]:
+             c.append(a[i]);
+             i+=1;
+         elif(a[i]>=b[j]):
+             c.append(b[j]);
+             j+=1;
+     while(i<lena):
+         c.append(a[i])
+         i+=1;
+     while(j<lenb):
+         c.append(b[j])
+         j+=1;
+     return c;                    
